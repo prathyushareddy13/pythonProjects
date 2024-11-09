1a5f6272eb4f86b12f7687eb4771f4be192f8c04
prathyushareddy13 <110794525+prathyushareddy13@users.noreply.github.com>
2024-11-09 22:43:15 +0530

    modified

--- November/ex_09112024/Literals_Task1.py
+++ November/ex_09112024/Literals_Task1.py
@@ -1,13 +1,34 @@
-num1=int(input("Enter the first number:"))
-num2=int(input("Enter the second number:"))
-num3=int(input("Enter the third number:"))
+try:
+    # Getting three inputs from user
+    input1 = int(input("Enter the first number:"))
+    input2 = int(input("Enter the second number:"))
+    input3 = int(input("Enter the third number:"))
 
-add = num1+num2+num3
-sub = num1-num2-num3
-mul = num1*num2*num3
-div = num1/num2/num3
+    # adding the inputs and storing them in a variable and printing the output
+    addition = input1 + input2 + input3
+    print("Sum of the numbers:\t", addition)
 
-print("Sum of the numbers:\t",add)
-print("Subtraction of the numbers:\t",sub)
-print("Multiplication of the numbers:\t",mul)
-print("Division of the numbers:\t",div)
+    # subtracting the inputs and storing them in a variable
+    # printing the output
+    subtraction = (input1 - input2) - input3
+    print("Subtraction of the numbers:\t", subtraction)
+
+    """ multiplying the inputs and storing them in a variable
+    printing the outputs """
+    multiplication = input1 * input2 * input3
+    print("Multiplication of the numbers:\t", multiplication)
+
+    """ dividing the inputs and storing them in a variable
+     printing the outputs"""
+    division = ((input1 / input2) / input3)
+    print("Division of the numbers:\t", division)
+
+# Handling exception if there is an input as 0
+except ZeroDivisionError:
+    # Printing the alert message
+    print("Please provide input value greater than Zero")
+
+# Handling exception for non integer inputs
+except:
+    # printing an informative message to change the input
+    print("Input is not a number, Please try again")

4a4c2bda72f22e134b2a62933c5484f9c5103066
prathyushareddy13 <110794525+prathyushareddy13@users.noreply.github.com>
2024-11-09 22:44:15 +0530

    modified

--- November/ex_09112024/QuotientandRemainder.py
+++ November/ex_09112024/QuotientandRemainder.py
@@ -1,7 +1,26 @@
 import math
-num1 = int(input("Enter Dividend:"))
-num2 = int(input("Enter Divisor:"))
-#print("Division of two numbers is:", num1/num2)
-quotient = math.floor(num1/num2)
-remainder = num1%num2
-print("Quotient is:",quotient,"\nRemainder is:",remainder)
+
+try:
+    # Accept Dividend from user
+    nDividend = int(input("Enter Dividend:"))
+
+    # Accept Divisor from user
+    nDivisor = int(input("Enter Divisor:"))
+
+    # Computing quotient by dividing Dividend and Divisor
+    nQuotient = math.floor(nDividend / nDivisor)
+
+    # Remainder is computed by getting the mod of Dividend and Divisor
+    nRemainder = nDividend % nDivisor
+
+    # Print the Quotient and Remainder
+    print("Quotient is:", nQuotient,"\nRemainder is:",nRemainder)
+
+except ZeroDivisionError:
+    # Handling exception when nDivisor is passed as 0
+    print("You can't divide by zero!")
+except:
+
+    # Handling exception when inputs are not a number
+    print("Input perhaps not a number!")
+

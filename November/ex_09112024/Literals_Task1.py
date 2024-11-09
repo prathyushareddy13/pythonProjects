7b547aa94a6eb5c66226dc2f362863b8f92c17dd
prathyushareddy13 <110794525+prathyushareddy13@users.noreply.github.com>
2024-11-09 08:41:40 +0530

    Literals done

---
+++ November/ex_09112024/Literals_Task1.py
@@ -0,0 +1,13 @@
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+num3=int(input("Enter the third number:"))
+
+add = num1+num2+num3
+sub = num1-num2-num3
+mul = num1*num2*num3
+div = num1/num2/num3
+
+print("Sum of the numbers:\t",add)
+print("Subtraction of the numbers:\t",sub)
+print("Multiplication of the numbers:\t",mul)
+print("Division of the numbers:\t",div)

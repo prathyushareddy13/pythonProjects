aab96d9b299ad644e89b4bf4cf5ae0d1afbb60ca
prathyushareddy13 <110794525+prathyushareddy13@users.noreply.github.com>
2024-11-09 21:44:28 +0530

    Quotient and remainder

--- November/ex_09112024/QuotientandRemainder.py
+++ November/ex_09112024/QuotientandRemainder.py
@@ -1,7 +1,7 @@
-
+import math
 num1 = int(input("Enter Dividend:"))
 num2 = int(input("Enter Divisor:"))
 #print("Division of two numbers is:", num1/num2)
-quotient = num1//num2
+quotient = math.floor(num1/num2)
 remainder = num1%num2
 print("Quotient is:",quotient,"\nRemainder is:",remainder)
